8e183dff4e9685f420367d14a67d691efc01ea6a
Vandebrewski <Bastiaan@MacBook-Pro-van-Bastiaan.local>
2014-04-25 20:28:46 +0200

    first commit

---
+++ js/controllers.js
@@ -0,0 +1,18 @@
+angular.module('starter.controllers', [])
+
+.controller('AppCtrl', function($scope) {
+})
+
+.controller('PlaylistsCtrl', function($scope) {
+  $scope.playlists = [
+    { title: 'Ionic', id: 1 },
+    { title: 'This is a test for comparison purposes', id: 2 },
+    { title: 'Dubstep', id: 3 },
+    { title: 'Indie', id: 4 },
+    { title: 'Rap', id: 5 },
+    { title: 'Cowbell', id: 6 }
+  ];
+})
+
+.controller('PlaylistCtrl', function($scope, $stateParams) {
+})
